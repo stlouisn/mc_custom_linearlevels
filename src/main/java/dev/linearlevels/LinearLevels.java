8bed898edd395e04dd15ec51c46ef5964a9f6202
stlouisn <stlouisn@hotmail.com>
2024-09-12 10:31:52 -0300

    Update LinearLevels.java

--- src/main/java/dev/linearlevels/LinearLevels.java
+++ src/main/java/dev/linearlevels/LinearLevels.java
@@ -1,11 +1,11 @@
 package dev.linearlevels;
 
-import dev.linearlevels.config.LinearLevelsConfigData;
+import dev.linearlevels.config.ModConfig;
+import dev.linearlevels.config.ModConfigData;
 import me.shedaniel.autoconfig.AutoConfig;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import org.slf4j.Logger;
@@ -17,17 +17,12 @@ public final class LinearLevels {
 
   public static final Logger LOGGER = LoggerFactory.getLogger(Constants.MOD_ID);
 
-  public LinearLevels(IEventBus modBus) {
+  public ModName(IEventBus modBus) {
     modBus.addListener(this::commonSetup);
-    modBus.addListener(this::clientSetup);
-    ModLoadingContext.get().registerExtensionPoint(IConfigScreenFactory.class, () -> (client, parent) -> AutoConfig.getConfigScreen(LinearLevelsConfigData.class, parent).get());
+    ModLoadingContext.get().registerExtensionPoint(IConfigScreenFactory.class, () -> (client, parent) -> AutoConfig.getConfigScreen(ModConfigData.class, parent).get());
   }
 
   private void commonSetup(FMLCommonSetupEvent event) {
-    event.enqueueWork(LinearLevelsConfig::configInit);
-  }
-
-  private void clientSetup(FMLClientSetupEvent event) {
-    event.enqueueWork(LinearLevelsClient::clientInit);
+    event.enqueueWork(ModConfig::init);
   }
 }
