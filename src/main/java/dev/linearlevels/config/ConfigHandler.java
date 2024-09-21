52d84f346c9c5fd178895013acbc0395eae8669f
stlouisn <stlouisn@hotmail.com>
2024-09-21 11:47:09 -0300

    added ConfigHandler file.commments

--- src/main/java/dev/linearlevels/config/ConfigHandler.java
+++ src/main/java/dev/linearlevels/config/ConfigHandler.java
@@ -7,20 +7,24 @@ public final class ConfigHandler {
 
   public static final ModConfigSpec configSpec;
 
+  public static ConfigValue<Integer> baseXPForOneLevel;
   public static ConfigValue<Boolean> curveMode;
-  public static ConfigValue<Integer> baseXPForOneLevel, curveModeMultiplier;
+  public static ConfigValue<Integer> curveModeMultiplier;
 
   static {
 
     ModConfigSpec.Builder builder = new ModConfigSpec.Builder();
 
+    baseXPForOneLevel = builder
+        .comment("The base amount of experience required for each level.\nDefault: 40")
+        .define("baseXPForOneLevel", 40);
+
     curveMode = builder
+        .comment("Use curved mode to increase experience required per level.\nFormula: baseXPForOneLevel + (playerLevel * curveModeMultiplier)\nDefault: true")
         .define("curveMode", true);
 
-    baseXPForOneLevel = builder
-        .define("baseXPForOneLevel", 50);
-
     curveModeMultiplier = builder
+        .comment("The multiplier used in the Curve Mode calculation.\nDefault: 1")
         .define("curveModeMultiplier", 1);
 
     configSpec = builder.build();
